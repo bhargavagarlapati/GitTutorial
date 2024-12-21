411442172e49f0520549a4d464b3222c2754873e
Bhargava <bhargavagarlapati@gmail.com>
2024-12-21 20:55:49 +0530

    commit method m1

--- Test.py
+++ Test.py
@@ -1,11 +1,3 @@
-print('Hi')
-print('Hello')
-print('How are you')
-print('Bye')
-print('ABC')
-print('DEF')
-print('HIJ')
-print('KLM')
-print('NOP')
-print('QRS')
+def m1():
+    print("Hi how are you")
 
